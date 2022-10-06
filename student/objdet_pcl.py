c5876b1fa01f886fb725c1150ab4dbd5ca768ced
nkamata <nobuhide.kamata@woven-planet.global>
2022-10-06 10:01:26 +0000

    BEV map for height data.

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -178,8 +178,8 @@ def bev_from_pcl(lidar_pcl, configs):
     img_intensity = img_intensity.astype(np.uint8)
     
     # Just for debug.
-    cv2.imshow('img_intensity', img_intensity)
-    cv2.waitKey(0)
+    # cv2.imshow('img_intensity', img_intensity)
+    # cv2.waitKey(0)
     
     #######
     ####### ID_S2_EX2 END ####### 
@@ -191,13 +191,19 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX3")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    height_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
 
     ## step 2 : assign the height value of each unique entry in lidar_top_pcl to the height map 
     ##          make sure that each entry is normalized on the difference between the upper and lower height defined in the config file
     ##          use the lidar_pcl_top data structure from the previous task to access the pixels of the height_map
-
+    height_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = lidar_pcl_top[:, 2] / float(np.abs(configs.lim_z[1] - configs.lim_z[0]))
+    
     ## step 3 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
-
+    img_height = height_map * 256
+    img_height = img_height.astype(np.uint8)
+    cv2.imshow('img_height', img_height)
+    cv2.waitKey(0)
+    
     #######
     ####### ID_S2_EX3 END #######       
 
