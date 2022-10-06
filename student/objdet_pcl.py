c056d13bae685e74731069e94e94bbbf00c3c2a4
nkamata <nobuhide.kamata@woven-planet.global>
2022-10-06 06:15:29 +0000

    Modified to show range and intensity images.

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -62,11 +62,11 @@ def show_range_image(frame, lidar_name):
     # step 1 : extract lidar data and range image for the roof-mounted lidar
     lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0]
     ri = []
-    if len(lidar.ri_return1.range_image_compressed) > 0: 
+    if len(lidar.ri_return1.range_image_compressed) > 0: # use first response
         ri = dataset_pb2.MatrixFloat()
         ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
         ri = np.array(ri.data).reshape(ri.shape.dims)
-    
+        
     # step 2 : extract the range and the intensity channel from the range image
     ri_range = ri[:,:,0]
     ri_intensity = ri[:,:,1]
@@ -77,12 +77,19 @@ def show_range_image(frame, lidar_name):
     
     # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
     ri_range = ri_range * 255 / (np.amax(ri_range) - np.amin(ri_range))
+    img_range = ri_range.astype(np.uint8)
     
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
-    ri_intensity = np.percentile(ri_intensity,99)/2 * ri_intensity * 255 / (np.percentile(ri_intensity,99) - np.amin(ri_intensity))
-        
+    # remove outliers
+    ri_intensity[ri_intensity>np.percentile(ri_intensity,99)]=np.percentile(ri_intensity,99)
+    ri_intensity[ri_intensity<np.percentile(ri_intensity,1)]=np.percentile(ri_intensity,1)
+
+    # map the intensity channel onto 8-bit scale
+    ri_intensity = np.amax(ri_intensity)/2 * ri_intensity * 255 / (np.amax(ri_intensity) - np.amin(ri_intensity)) 
+    img_intensity = ri_intensity.astype(np.uint8)
+    
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
-    img_range_intensity = np.vstack([ri_range,ri_intensity]) 
+    img_range_intensity = np.vstack([img_range,img_intensity]) 
 
     #######
     ####### ID_S1_EX1 END #######     
